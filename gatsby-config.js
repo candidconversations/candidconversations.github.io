56824d2baa7664d4ff8f0989a77dcd20f20160cb
Candid Conversations <candidconversationsproject@gmail.com>
2021-01-11 07:54:48 -0400

    Gatsby helloworld

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "Candid Conversations",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
